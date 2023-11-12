b35f2b3681f4928c0d313c6b08c61c62439ab738
DayDreamer027 <DayDreamer030906@gmail.com>
2023-11-12 11:47:00 -0500

    Basic game features for Level1 (sun, cloud, and shadow behaviour, Collectable Item)

--- Assets/Cloud.cs
+++ Assets/Cloud.cs
@@ -6,6 +6,14 @@ public class Cloud : MonoBehaviour
 {
     public Transform left;
     public Transform right;
+    public float groundHeight = 200;
+    private float movedDistance;
+    public float shouldMovedDistance = 10;
+    public float floatSpeed = 2.5F;
+    public PolygonCollider2D shawdowCollider;
+    public MeshFilter shadowMesh;
+    private float timer = 0;
+    public float safeTime = 2;
     public Sun sun;
     // Start is called before the first frame update
     void Start()
@@ -16,7 +24,40 @@ public class Cloud : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Vector2 cloudPosition = Vector2.zero; //new Vector2(transform.position.x , transform.position.y);
 
+        Vector2 cloudLeft = getCloudLeft();
+        Vector2 cloudRight = getCloudRight();
+        Vector2 groundLeft = getPointOnGround(sun.getLightResource(), cloudLeft);
+        Vector2 groundRight = getPointOnGround(sun.getLightResource(), cloudRight);
+        Vector2[] shadowPoints = { groundLeft - cloudPosition, groundRight - cloudPosition, cloudRight - cloudPosition, cloudLeft - cloudPosition };
+        shawdowCollider.SetPath(0, shadowPoints);
+
+        timer += Time.deltaTime;
+        if(timer < 5) { //the safe period for player to move around
+            shadowMesh.mesh = shawdowCollider.CreateMesh(false, false);
+        }
+        else
+        {
+            shadowMesh.mesh = null;
+            if(timer > 5 + safeTime) { 
+                timer = 0;
+            }
+        }
+
+        if (movedDistance < shouldMovedDistance)
+        {
+            this.transform.transform.position = transform.position + (Vector3.left * floatSpeed * Time.deltaTime);
+        }
+        else
+        {
+            this.transform.transform.position = transform.position + (Vector3.right * floatSpeed * Time.deltaTime);
+            if (movedDistance > 2*shouldMovedDistance)
+            {
+                movedDistance = 0;
+            }
+        }
+        movedDistance += floatSpeed * Time.deltaTime;
     }
 
     Vector2 getCloudLeft()
@@ -24,9 +65,27 @@ public class Cloud : MonoBehaviour
         return (new Vector2(left.position.x, left.position.y));
     }
 
-    Vector2 getCloudRigth()
+    Vector2 getCloudRight()
     {
         return (new Vector2(right.position.x, right.position.y));
+    }
+
+    Vector2 getPointOnGround(Vector2 sunLightSource, Vector2 cloudEdge)
+    {
+        Vector2 lightDirec = sunLightSource - cloudEdge;
+        float cloudToGroundY = groundHeight - cloudEdge.y;
+        //lightDirec.Normalize();
+        float coefficient;
+        float y;
+        float a;
+        float b;
+        coefficient = sunLightSource.x - cloudEdge.x;
+        y = sunLightSource.y - cloudEdge.y;
+        a = y / coefficient; //
+        b = sunLightSource.y - coefficient * a;
+        float cloudToGroundX = cloudToGroundY / a;
+        return (new Vector2(cloudToGroundX + cloudEdge.x, groundHeight));
+        //return (new Vector2(-b / a, 0));
     }
 
 }
--- Assets/CollectableItem.cs
+++ Assets/CollectableItem.cs
@@ -4,8 +4,18 @@ using UnityEngine;
 
 public class CollectableItem : MonoBehaviour
 {
+    private Collider2D collisionArea;
+    private bool shouldRespawn = false;
+    public float timer = 0;
+    public float respawnTime = 1F;
+    private Vector3 currentPos;
+    public GameObject collectableItem; //the prefab you want to spawn
     private Object thisObject;
-    
+
+    //public GameObject ; //the prefab you want to spawn
+    private GameObject spawnedball; //a local temporary reference
+
+
     public void Awake()
     {
         thisObject = GetComponent<Object>();
@@ -19,14 +29,39 @@ public class CollectableItem : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(shouldRespawn)
+        {
+            timer += Time.deltaTime;
+            if(timer > respawnTime)
+            {
+                //thisObject = new CollectableItem(); //or set multiple boolean variables for more items
+                //this.transform.position = new Vector3(2, 20, 0);
+                
+                Vector3 newPos = new Vector3(currentPos.x - 5, currentPos.y, 0);
+                //GameObject collectableItem = Instantiate(collectableItem, newPos, Quaternion.identity);
+                //collectableItem = (GameObject)Instantiate(collectableItem, newPos, new Quaternion());
+                //gameObject.transform.SetPositionAndRotation(gameObject.transform.position.x) -= 5;
+                //Vector3 newPos = new Vector3(gameObject.transform.position.x - 5, gameObject.transform.position.y, gameObject.transform.position.z);
+                //gameObject.transform.position = newPos;
+                //gameObject.SetActive(true);
+                shouldRespawn = false;
+                timer = 0;
+            }
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("PlayerCollider"))
+        if (collision.CompareTag("Player")) //need set the player with PlayerCollider tag to make it work
         {
-            Destroy(gameObject);
+            shouldRespawn = true;
+            //Vector3 newPos = new Vector3(gameObject.transform.position.x - 5, gameObject.transform.position.y, 0);
+            //gameObject.transform.position = newPos;
+            currentPos = transform.position;
+            Vector3 newPos = new Vector3(currentPos.x - 5, currentPos.y, currentPos.z);
+            this.transform.position = newPos;
+            //Destroy(gameObject);
+            //gameObject.SetActive(false); // Unity will no longer call Update() method
         }
     }
 }
